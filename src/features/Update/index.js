337c42e43984366ef62699f5dd74c86919bb8d93
Kuba Tuta <tuta.jakub@gmail.com>
2026-05-17 19:17:58 +0200

    file input and mounting handler

--- src/features/Update/index.js
+++ src/features/Update/index.js
@@ -1,5 +1,21 @@
+import { useState } from "react";
+
 function Update() {
+
+    const [file, setFile] = useState([null, null, null]);
+
+    const handleInput = (event, number) => {
+    setFile((prevFile) => {
+      const fileArray = [...prevFile];
+      fileArray[number] = event.target.files[0];
+      return fileArray;
+    });
+  };
     return (
-        <>update</>
+        <div>
+            <form>
+                <input type="file" onChange={(event) => handleInput(event, 0)}/>
+            </form>
+        </div>
     )
 } export default Update;
