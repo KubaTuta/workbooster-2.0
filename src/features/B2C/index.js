0254d15fbbd2d101e36cd3ad91b72149321220e3
Kuba Tuta <tuta.jakub@gmail.com>
2026-05-19 20:25:03 +0200

    first render divs

--- src/features/B2C/index.js
+++ src/features/B2C/index.js
@@ -1,3 +1,17 @@
+import { Layout, Tile } from "./styled";
+
 function B2C() {
-  return <>b2c</>;
-} export default B2C;
+  const parsedData = JSON.parse(localStorage.getItem("ekspertyzy")) || [];
+
+  return (
+    <Layout>
+      {parsedData.map((singleVehicle) => (
+        <>
+          <Tile>{singleVehicle.plate}</Tile>
+          <Tile>{singleVehicle.vin}</Tile>
+        </>
+      ))}
+    </Layout>
+  );
+}
+export default B2C;
