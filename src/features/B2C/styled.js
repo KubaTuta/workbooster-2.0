40c224a0d4f76ad72fd10daff37b0b776dfff537
Kuba Tuta <tuta.jakub@gmail.com>
2026-05-19 20:25:16 +0200

    grid

---
+++ src/features/B2C/styled.js
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+export const Layout = styled.div`
+  display: grid;
+  grid-template-columns: max-content max-content;
+  grid-gap: 5px;
+  margin: 10px;
+`;
+
+export const Tile = styled.div`
+  background-color: #076f4b;
+  color: white;
+  border-radius: 5px;
+  //   transition: 0.5s;
+  align-items: center;
+  padding: 2px;
+  //   word-break: break-word;
+  //   min-width: 0;
+  min-width: 0;
+  width: 100%;
+`;
