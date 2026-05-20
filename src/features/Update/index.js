51423f8ac20a800aa2e4ef1a221ef5fcc03d063b
Kuba Tuta <tuta.jakub@gmail.com>
2026-05-20 16:46:49 +0200

    second file reader

--- src/features/Update/index.js
+++ src/features/Update/index.js
@@ -1,9 +1,9 @@
 import { useState } from "react";
-import { ewiMap } from "./dataMaps";
+import { ewiMap, macadamMap } from "./dataMaps";
 import { handleConvertRecords } from "../../hooks";
 
 function Update() {
-  const [file, setFile] = useState([null]);
+  const [file, setFile] = useState([null, null]);
 
   function handleInput(event, number) {
     setFile((prevFile) => {
@@ -16,12 +16,23 @@ function Update() {
   return (
     <div>
       <form>
+        <>
         <input type="file" onChange={(event) => handleInput(event, 0)} />
         {file[0] !== null ? (
-          <button onClick={() => handleConvertRecords(0, ewiMap, "Ewindencja", file)}>Zapisz</button>
+          <button onClick={() => handleConvertRecords(0, ewiMap, "Ewidencja", file)}>Zapisz</button>
         ) : (
           ""
         )}
+        </>
+        <>
+        <input type="file" onChange={(event) => handleInput(event, 1)} />
+        {file[1] !== null ? (
+          <button onClick={() => handleConvertRecords(1, macadamMap, "Ekspertyzy", file)}>Zapisz</button>
+        ) : (
+          ""
+        )}
+        </>
+        
       </form>
     </div>
   );
