5ed7dc0b8586a53e7d9620681696d240a531f8b1
Kuba Tuta <tuta.jakub@gmail.com>
2026-05-20 16:45:48 +0200

    merging filtered data to render it

--- src/features/B2C/index.js
+++ src/features/B2C/index.js
@@ -1,14 +1,63 @@
 import { Layout, Tile } from "./styled";
 
 function B2C() {
-  const parsedData = JSON.parse(localStorage.getItem("ekspertyzy")) || [];
+  const parsedEwi = JSON.parse(localStorage.getItem("Ewidencja")) || [];
+  const parsedMacadam = JSON.parse(localStorage.getItem("Ekspertyzy")) || [];
+
+  const macadamMap = new Map(parsedMacadam.map((item) => [item.plate, item]));
+
+  const renderedData = parsedEwi
+    .filter((singleVehicle) => macadamMap.has(singleVehicle.plate))
+    .map((singleVehicle) => {
+      const macadamVehicle = macadamMap.get(singleVehicle.plate);
+      return {
+        plate: singleVehicle.plate,
+        vin: singleVehicle.vin,
+        company: singleVehicle.company,
+        collectionDate: singleVehicle.collectionDate,
+        brand: singleVehicle.brand,
+        body: singleVehicle.body,
+        version: singleVehicle.version,
+        fuel: singleVehicle.fuel,
+        color: singleVehicle.color,
+        mileage: singleVehicle.mileage,
+        prodYear: singleVehicle.prodYear,
+        firstRegNb: singleVehicle.firstRegNb,
+        damage: singleVehicle.damage,
+        comment: singleVehicle.comment,
+        eurotax: singleVehicle.eurotax,
+        eurotaxDate: singleVehicle.eurotaxDate,
+        reservation: singleVehicle.reservation,
+        rvStat: singleVehicle.rvStat,
+        refurbishment: singleVehicle.refurbishment,
+        link: macadamVehicle.macadamWithPrice,
+      }
+    });
 
   return (
     <Layout>
-      {parsedData.map((singleVehicle) => (
+      {renderedData.map((singleVehicle) => (
         <>
           <Tile>{singleVehicle.plate}</Tile>
           <Tile>{singleVehicle.vin}</Tile>
+          <Tile>{singleVehicle.company}</Tile>
+          <Tile>{singleVehicle.collectionDate}</Tile>
+          <Tile>{singleVehicle.brand}</Tile>
+          <Tile>{singleVehicle.body}</Tile>
+          <Tile>{singleVehicle.version}</Tile>
+          <Tile>{singleVehicle.color}</Tile>
+          <Tile>{singleVehicle.mileage}</Tile>
+          <Tile>{singleVehicle.prodYear}</Tile>
+          <Tile>{singleVehicle.firstRegNb}</Tile>
+          <Tile>{singleVehicle.damage}</Tile>
+          <Tile>{singleVehicle.comment}</Tile>
+          <Tile>{singleVehicle.eurotax}</Tile>
+          <Tile>{singleVehicle.eurotaxDate}</Tile>
+          <Tile>{singleVehicle.reservation}</Tile>
+          <Tile>{singleVehicle.rvStat}</Tile>
+          <Tile>{singleVehicle.refurbishment}</Tile>
+          <Tile>{singleVehicle.link}</Tile>
+         
         </>
       ))}
     </Layout>
