3dae2542201beaa95efa47d8c1feaa2f0dbb9fe8
Kuba Tuta <tuta.jakub@gmail.com>
2026-05-20 13:33:00 +0200

    data structures outside of component

--- src/features/Update/index.js
+++ src/features/Update/index.js
@@ -1,71 +1,70 @@
 import { useState } from "react";
+import { ewiMap } from "./dataMaps";
 var XLSX = require("xlsx");
 
 function Update() {
   const [file, setFile] = useState([null]);
-
-  const columnPosition = {
-    plate: { column: 0 },
-    vin: { column: 1 },
-    status: { column: 2 },
-    company: { column: 3 },
-    collectionDate: {
-      column: 4,
-      isDate: true,
-    },
-    brand: { column: 5 },
-    model: { column: 6 },
-    body: { column: 7 },
-    version: { column: 8 },
-    fuel: { column: 9 },
-    color: { column: 10 },
-    mileage: { column: 11 },
-    prodYear: { column: 12 },
-    firstRegDate: {
-      column: 13,
-      isDate: true,
-    },
-    keysNb: { column: 15 },
-    damages: { column: 17 },
-    comment: { column: 18 },
-    eurotaxValue: { column: 19 },
-    eurotaxNb: { column: 20 },
-    eurotaxDate: {
-      column: 21,
-      isDate: true,
-    },
-    reservation: { column: 22 },
-    rvStat: { column: 23 },
-    auctionPricePlusFee: { column: 24 },
-    fee: { column: 25 },
-    salesPriceNoFee: { column: 26 },
-    salesChannel: { column: 30 },
-    buyersData: { column: 31 },
-    vatId: { column: 32 },
-    proformaDate: {
-      column: 33,
-      isDate: true,
-    },
-    paymentDate: {
-      column: 34,
-      isDate: true,
-    },
-    nac: { column: 35 },
-    invoiceNb: { column: 36 },
-    invoiceDate: {
-      column: 37,
-      isDate: true,
-    },
-    insurer: { column: 39 },
-    ocExpirationDate: {
-      column: 40,
-      isDate: true,
-    },
-    refurbrishment: { column: 45 },
-    b2cPrice: { column: 47 },
-    priceAcceptance: { column: 51 },
-    location: { column: 60 },
-  };
+  //   plate: { column: 0 },
+  //   vin: { column: 1 },
+  //   status: { column: 2 },
+  //   company: { column: 3 },
+  //   collectionDate: {
+  //     column: 4,
+  //     isDate: true,
+  //   },
+  //   brand: { column: 5 },
+  //   model: { column: 6 },
+  //   body: { column: 7 },
+  //   version: { column: 8 },
+  //   fuel: { column: 9 },
+  //   color: { column: 10 },
+  //   mileage: { column: 11 },
+  //   prodYear: { column: 12 },
+  //   firstRegDate: {
+  //     column: 13,
+  //     isDate: true,
+  //   },
+  //   keysNb: { column: 15 },
+  //   damages: { column: 17 },
+  //   comment: { column: 18 },
+  //   eurotaxValue: { column: 19 },
+  //   eurotaxNb: { column: 20 },
+  //   eurotaxDate: {
+  //     column: 21,
+  //     isDate: true,
+  //   },
+  //   reservation: { column: 22 },
+  //   rvStat: { column: 23 },
+  //   auctionPricePlusFee: { column: 24 },
+  //   fee: { column: 25 },
+  //   salesPriceNoFee: { column: 26 },
+  //   salesChannel: { column: 30 },
+  //   buyersData: { column: 31 },
+  //   vatId: { column: 32 },
+  //   proformaDate: {
+  //     column: 33,
+  //     isDate: true,
+  //   },
+  //   paymentDate: {
+  //     column: 34,
+  //     isDate: true,
+  //   },
+  //   nac: { column: 35 },
+  //   invoiceNb: { column: 36 },
+  //   invoiceDate: {
+  //     column: 37,
+  //     isDate: true,
+  //   },
+  //   insurer: { column: 39 },
+  //   ocExpirationDate: {
+  //     column: 40,
+  //     isDate: true,
+  //   },
+  //   refurbrishment: { column: 45 },
+  //   b2cPrice: { column: 47 },
+  //   priceAcceptance: { column: 51 },
+  //   location: { column: 60 },
+  // };
 
   function handleInput(event, number) {
     setFile((prevFile) => {
@@ -97,7 +96,7 @@ function Update() {
             id: i + 1,
           };
 
-          Object.entries(columnPosition).forEach(([carProperty, config]) => {
+          Object.entries(ewiMap).forEach(([carProperty, config]) => {
             const cell = worksheet[XLSX.utils.encode_cell({ r: i, c: config.column })];
 
             let cellValue = cell ? cell.v : undefined;
