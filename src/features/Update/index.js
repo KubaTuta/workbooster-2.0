d5f72b7453dd23bf8a0d8f85a43a461dc85e22f6
Kuba Tuta <tuta.jakub@gmail.com>
2026-05-20 12:17:19 +0200

    extend configuration of single car properties, like a date ex.

--- src/features/Update/index.js
+++ src/features/Update/index.js
@@ -5,45 +5,66 @@ function Update() {
   const [file, setFile] = useState([null]);
 
   const columnPosition = {
-    plate: 0,
-    vin: 1,
-    status: 2,
-    company: 3,
-    collectionDate: 4,
-    brand: 5,
-    model: 6,
-    body: 7,
-    version: 8,
-    fuel: 9,
-    color: 10,
-    mileage: 11,
-    prodYear: 12,
-    firstRegDate: 13,
-    keysNb: 15,
-    damages: 17,
-    comment: 18,
-    eurotaxValue: 19,
-    eurotaxNb: 20,
-    eurotaxDate: 21,
-    reservation: 22,
-    rvStat: 23,
-    auctionPricePlusFee: 24,
-    fee: 25,
-    salesPriceNoFee: 26,
-    salesChannel: 30,
-    buyersData: 31,
-    vatId: 32,
-    proformaDate: 33,
-    paymentDate: 34,
-    nac: 35,
-    invoiceNb: 36,
-    invoiceDate: 37,
-    insurer: 39,
-    ocExpirationDate: 40,
-    refurbrishment: 45,
-    b2cPrice: 47,
-    priceAcceptance: 51,
-    location: 60,
+    plate: { column: 0 },
+    vin: { column: 1 },
+    status: { column: 2 },
+    company: { column: 3 },
+    collectionDate: {
+      column: 4,
+      isDate: true,
+    },
+    brand: { column: 5 },
+    model: { column: 6 },
+    body: { column: 7 },
+    version: { column: 8 },
+    fuel: { column: 9 },
+    color: { column: 10 },
+    mileage: { column: 11 },
+    prodYear: { column: 12 },
+    firstRegDate: {
+      column: 13,
+      isDate: true,
+    },
+    keysNb: { column: 15 },
+    damages: { column: 17 },
+    comment: { column: 18 },
+    eurotaxValue: { column: 19 },
+    eurotaxNb: { column: 20 },
+    eurotaxDate: {
+      column: 21,
+      isDate: true,
+    },
+    reservation: { column: 22 },
+    rvStat: { column: 23 },
+    auctionPricePlusFee: { column: 24 },
+    fee: { column: 25 },
+    salesPriceNoFee: { column: 26 },
+    salesChannel: { column: 30 },
+    buyersData: { column: 31 },
+    vatId: { column: 32 },
+    proformaDate: {
+      column: 33,
+      isDate: true,
+    },
+    paymentDate: {
+      column: 34,
+      isDate: true,
+    },
+    nac: { column: 35 },
+    invoiceNb: { column: 36 },
+    invoiceDate: {
+      column: 37,
+      isDate: true,
+    },
+    insurer: { column: 39 },
+    ocExpirationDate: {
+      column: 40,
+      isDate: true,
+    },
+    refurbrishment: { column: 45 },
+    b2cPrice: { column: 47 },
+    priceAcceptance: { column: 51 },
+    location: { column: 60 },
   };
 
   function handleInput(event, number) {
@@ -72,8 +93,8 @@ function Update() {
             id: i + 1,
           };
 
-          Object.entries(columnPosition).forEach(([carProperty, column]) => {
-            const cell = worksheet[XLSX.utils.encode_cell({ r: i, c: column })];
+          Object.entries(columnPosition).forEach(([carProperty, config]) => {
+            const cell = worksheet[XLSX.utils.encode_cell({ r: i, c: config.column })];
             singleCar[carProperty] = cell ? cell.v : undefined;
           });
 
