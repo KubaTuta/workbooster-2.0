68e5211f0d6e383b2db824749d1e52476c387c62
Kuba Tuta <tuta.jakub@gmail.com>
2026-05-20 16:46:03 +0200

    more columns

--- src/features/B2C/styled.js
+++ src/features/B2C/styled.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const Layout = styled.div`
   display: grid;
-  grid-template-columns: max-content max-content;
+  grid-template-columns: repeat(19, max-content);
   grid-gap: 5px;
   margin: 10px;
 `;
