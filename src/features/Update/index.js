3be68f79c2d5b85a43a265cd2ebd9ec17bfc9703
Kuba Tuta <tuta.jakub@gmail.com>
2026-05-20 10:59:35 +0200

    data position in given file

--- src/features/Update/index.js
+++ src/features/Update/index.js
@@ -4,6 +4,48 @@ var XLSX = require("xlsx");
 function Update() {
   const [file, setFile] = useState([null]);
 
+  const columnPosition = {
+    plate: 0,
+    vin: 1,
+    status: 2,
+    company: 3,
+    collectionDate: 4,
+    brand: 5,
+    model: 6,
+    body: 7,
+    version: 8,
+    fuel: 9,
+    color: 10,
+    mileage: 11,
+    prodYear: 12,
+    firstRegDate: 13,
+    keysNb: 15,
+    damages: 17,
+    comment: 18,
+    eurotaxValue: 19,
+    eurotaxNb: 20,
+    eurotaxDate: 21,
+    reservation: 22,
+    rvStat: 23,
+    auctionPricePlusFee: 24,
+    fee: 25,
+    salesPriceNoFee: 26,
+    salesChannel: 30,
+    buyersData: 31,
+    vatId: 32,
+    proformaDate: 33,
+    paymentDate: 34,
+    nac: 35,
+    invoiceNb: 36,
+    invoiceDate: 37,
+    insurer: 39,
+    ocExpirationDate: 40,
+    refurbrishment: 45,
+    b2cPrice: 47,
+    priceAcceptance: 51,
+    location: 60,
+  };
+
   function handleInput(event, number) {
     setFile((prevFile) => {
       const fileArray = [...prevFile];
@@ -28,10 +70,12 @@ function Update() {
         for (let i = 0; i <= rows; i++) {
           const cellA = worksheet[XLSX.utils.encode_cell({ r: i, c: 0 })];
           const cellB = worksheet[XLSX.utils.encode_cell({ r: i, c: 1 })];
+          const cellC = worksheet[XLSX.utils.encode_cell({ r: i, c: 2 })];
 
           if (cellA) {
             const plate = cellA.v;
             const vin = cellB ? cellB.v : undefined;
+            const status = cellC ? cellC.v : undefined;
 
             const finalDataObject = {
               id: i + 1,
@@ -54,7 +98,6 @@ function Update() {
         <input type="file" onChange={(event) => handleInput(event, 0)} />
         {file[0] !== null ? (
           <button onClick={() => handleConvertRecords()}>Zapisz</button>
-          
         ) : (
           ""
         )}
