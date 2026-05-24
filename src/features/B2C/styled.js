d5bfa4b2980ed01ee70f5010173e525373cad5d9
Kuba Tuta <tuta.jakub@gmail.com>
2026-05-24 20:01:19 +0200

    maximum div size

--- src/features/B2C/styled.js
+++ src/features/B2C/styled.js
@@ -2,7 +2,8 @@ import styled from "styled-components";
 
 export const Layout = styled.div`
   display: grid;
-  grid-template-columns: repeat(20, max-content);
+  grid-template-columns: repeat(20, minmax(0, auto));
+  width: max-content;
   grid-gap: 5px;
   margin: 10px;
 `;
@@ -11,11 +12,9 @@ export const Tile = styled.div`
   background-color: #076f4b;
   color: white;
   border-radius: 5px;
-  //   transition: 0.5s;
   align-items: center;
   padding: 2px;
-  //   word-break: break-word;
-  //   min-width: 0;
-  min-width: 0;
+  max-width: 100px;
+  min-width: 0px;
   width: 100%;
 `;
