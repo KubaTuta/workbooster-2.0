ac0175cd16071cf2321812bcd891e71ef2b892de
Kuba Tuta <tuta.jakub@gmail.com>
2026-05-25 19:09:00 +0200

    remove strict mode

--- src/index.js
+++ src/index.js
@@ -8,12 +8,10 @@ import { HashRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <HashRouter>
-      <GlobalStyles />
-      <App />   
-    </HashRouter>
-  </React.StrictMode>,
+  <HashRouter>
+    <GlobalStyles />
+    <App />
+  </HashRouter>,
 );
 
 reportWebVitals();
