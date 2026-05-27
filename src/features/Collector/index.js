c746a6a83d786d79c9bf29cc9724097c5b963a0d
Kuba Tuta <tuta.jakub@gmail.com>
2026-05-27 19:10:39 +0200

    function that divide text to words and concat them to car plates

--- src/features/Collector/index.js
+++ src/features/Collector/index.js
@@ -4,6 +4,34 @@ import { useState } from "react";
 function Collector() {
   const [textarea, setTextarea] = useState("");
 
+  function platePicker() {
+    const singleWords = textarea.replace(/[^a-zA-Z0-9]/g, " ").split(/\s+/);
+
+    const formattedWords = singleWords.reduce((acc, word, index, arr) => {
+      if (
+        /[a-zA-Z]/.test(word[0]) &&
+        (word.length === 7 || word.length === 8)
+      ) {
+        acc.push(word);
+      } else if (
+        /[a-zA-Z]/.test(word[0]) &&
+        word.length < 7 &&
+        arr[index + 1]
+      ) {
+        const combined2words = word + arr[index + 1];
+        if (combined2words.length === 7 || combined2words.length === 8) {
+          acc.push(combined2words);
+        } else if (combined2words.length < 7 && arr[index + 2]) {
+          const combined3words = combined2words + arr[index + 2];
+          (combined3words.length === 7 || combined3words.length === 8) &&
+            acc.push(combined3words);
+        }
+      }
+      return acc;
+    }, []);
+    setPlates(formattedWords);
+  }
+
   return (
     <Layout>
       <Frame>
@@ -11,7 +39,7 @@ function Collector() {
           value={textarea}
           onChange={(e) => setTextarea(e.target.value)}
         ></TextWindow>
-        <ConfirmationButton>button</ConfirmationButton>
+        <ConfirmationButton onClick={platePicker}>OK</ConfirmationButton>
       </Frame>
     </Layout>
   );
