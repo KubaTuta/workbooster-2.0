9ffd00aac83fb3f501e236c7584f28023ee44a85
Kuba Tuta <tuta.jakub@gmail.com>
2026-05-27 13:00:55 +0200

    controlled textarea

--- src/features/Collector/index.js
+++ src/features/Collector/index.js
@@ -1,12 +1,16 @@
 import { Layout, Frame, TextWindow, ConfirmationButton } from "./styled";
+import { useState } from "react";
 
 function Collector() {
-    
+  const [textarea, setTextarea] = useState("");
 
   return (
     <Layout>
       <Frame>
-        <TextWindow></TextWindow>
+        <TextWindow
+          value={textarea}
+          onChange={(e) => setTextarea(e.target.value)}
+        ></TextWindow>
         <ConfirmationButton>button</ConfirmationButton>
       </Frame>
     </Layout>
