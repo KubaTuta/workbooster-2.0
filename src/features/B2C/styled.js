9f60db6f3ca8310649181b46cbcb6945abdec142
Kuba Tuta <tuta.jakub@gmail.com>
2026-06-01 20:24:06 +0200

    style divided

--- src/features/B2C/styled.js
+++ src/features/B2C/styled.js
@@ -24,22 +24,6 @@ export const Header = styled.div`
   text-overflow: ellipsis;
 `;
 
-export const Tile = styled.div`
-  // background-color: #14513c;
-  background-color: #093f45;
-  font-size: 15px;
-  color: white;
-  border-radius: 5px;
-  align-items: center;
-  padding: 2px;
-  max-width: 100px;
-  min-width: 0px;
-  width: 100%;
-  white-space: nowrap;
-  overflow: hidden;
-  text-overflow: ellipsis;
-`;
-
 export const Tooltip = styled.div`
   font-size: 26px;
   border: 3px solid black;
@@ -52,20 +36,4 @@ export const Tooltip = styled.div`
   z-index: 9999;
   padding: 5px;
   border-radius: 10px;
-`;
-
-export const Button = styled.button`
-  background-color: #14513c;
-  cursor: pointer;
-  font-size: 15px;
-  color: white;
-  border-radius: 5px;
-  align-items: center;
-  padding: 2px;
-  max-width: 100px;
-  min-width: 0px;
-  width: 100%;
-  white-space: nowrap;
-  overflow: hidden;
-  text-overflow: ellipsis;
 `;
---
+++ src/features/CarExpander/styled.js
@@ -0,0 +1,45 @@
+import styled from "styled-components";
+
+export const Row = styled.div`
+  display: contents;
+`;
+
+export const Tile = styled.div`
+  // background-color: #14513c;
+  background-color: #093f45;
+  font-size: 15px;
+  color: white;
+  border-radius: 5px;
+  align-items: center;
+  padding: 2px;
+  max-width: 100px;
+  min-width: 0px;
+  width: 100%;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+`;
+
+export const Button = styled.button`
+  background-color: #14513c;
+  cursor: pointer;
+  font-size: 15px;
+  color: white;
+  border-radius: 5px;
+  align-items: center;
+  padding: 2px;
+  max-width: 100px;
+  min-width: 0px;
+  width: 100%;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+`;
+
+export const Details = styled.div`
+  grid-column: 1 / -1;
+  background: #1b263b;
+  color: white;
+  padding: 20px;
+  margin-bottom: 5px;
+`;
