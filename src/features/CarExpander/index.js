4923bb6e145f48b61c861930252f8d67ef2e40e9
Kuba Tuta <tuta.jakub@gmail.com>
2026-06-02 12:18:40 +0200

    damages expanded

--- src/features/CarExpander/index.js
+++ src/features/CarExpander/index.js
@@ -74,7 +74,20 @@ function CarExpander({ render, hovered, setHovered }) {
                 </Tile>
               );
           })}
-          {expandedCar === car.plate && <Details>rozwinięte</Details>}
+          {expandedCar === car.plate &&
+            (car.damages.length > 0 ? (
+              <Details>
+                {car.damages.map((property) =>
+                  Object.entries(property).map(([key, value]) => (
+                    <div>
+                      {key}: {value}
+                    </div>
+                  )),
+                )}
+              </Details>
+            ) : (
+              <Details>pozostałe</Details>
+            ))}
         </Row>
       ))}
     </>
