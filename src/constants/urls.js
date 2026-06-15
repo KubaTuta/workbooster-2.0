e2622277e81a177af2017f32f4abf703afba2539
Kuba Tuta <tuta.jakub@gmail.com>
2026-06-15 09:42:20 +0200

    url to sharepoint

--- src/constants/urls.js
+++ src/constants/urls.js
@@ -1,3 +1,5 @@
 export function damageUrl(plate) {
   return `https://serwisarval.pl/claims/insurancecase/index/page/1?claim_number=&contract_plate_number=${plate}&claim_number_insurance_company=&client_name=&claim_date_from=&claim_date_to=&type=&case_closed=&special_care=&gaps_filled=&submitFilterForm=Filtruj`;
 }
+
+export const macadamUrl = "https://bnpparibas.sharepoint.com/:x:/r/sites/RemarketingPoland/Shared%20Documents/Kuba%20T/booster/ekspertyzy.xlsx?d=w23fe7fc9c5b845c2828a1a207b622fdb&csf=1&web=1&e=dlGSMM";
