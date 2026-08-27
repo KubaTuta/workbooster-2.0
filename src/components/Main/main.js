8876036ea033b5a53073aab6467435d4d47d572b
Kuba Tuta <tuta.jakub@gmail.com>
2026-08-27 15:34:58 +0200

    model moved into Statuses

--- src/components/Main/main.js
+++ src/components/Main/main.js
@@ -1,12 +1,7 @@
-import { useCarsToSellViewModel } from "../../hooks/useCarsToSellViewModel";
 import Collector from "../Common/collector";
 import Statuses from "./statuses";
 
 function Main({ plates, setPlates }) {
-  const carsToSellViewModel = useCarsToSellViewModel(plates);
-
-  console.log(carsToSellViewModel);
-
   return (
     <>
       <Collector setPlates={setPlates} />
